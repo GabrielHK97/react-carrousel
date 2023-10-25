8e26e76d06b1cc475084fc6bf2cf35c8bec8a485
Gabriel Kubota <gabrielkhiroshi@gmail.com>
2023-10-25 19:56:35 -0300

    introducao do conceito

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Carrousel from './components/carrousel';
+import cat1 from "./lib/assets/1.jpeg";
+import cat2 from "./lib/assets/2.jpeg";
+import cat3 from "./lib/assets/3.jpeg";
+import cat4 from "./lib/assets/4.jpeg";
+import cat5 from "./lib/assets/5.jpeg";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-pink-400">
+      <Carrousel className='w-96 h-96' images={[cat1,cat2,cat3,cat4,cat5]}/>
     </div>
   );
 }
---
+++ src/components/carrousel.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import "../App.css";
+
+interface Props {
+  className?: string;
+  seconds?: number;
+  images: string[];
+}
+
+function Carrousel({
+  className = "w-full h-full",
+  seconds = 0.2,
+  images,
+}: Props) {
+  const [index, setIndex] = useState(0);
+  const [nextIndex, setNexIndex] = useState(0);
+  const [translate, setTranslate] = useState("0%");
+  const [secs, setSecs] = useState(0);
+
+  function resetIndex(i: number): number {
+    if (Math.abs(i) > images.length - 1) {
+      return i % images.length;
+    } else {
+      return i;
+    }
+  }
+
+  function goRight() {
+    setNexIndex(1);
+    setTranslate("-100%");
+    setSecs(seconds);
+    setTimeout(() => {
+      setIndex(resetIndex(index + 1));
+      setTranslate("0%");
+      setSecs(0);
+    }, seconds * 1000);
+  }
+
+  function goLeft() {
+    setNexIndex(-1);
+    setTranslate("100%");
+    setSecs(seconds);
+    setTimeout(() => {
+      setIndex(resetIndex(index - 1));
+      setTranslate("0%");
+      setSecs(0);
+    }, seconds * 1000);
+  }
+
+  return (
+    <div className={className}>
+      <div className="w-full h-full overflow-hidden flex">
+        <div
+          className={"w-full h-full flex"}
+          style={{
+            transform: `translateX(${translate})`,
+            transition: `${secs}s`,
+            flexDirection: `${nextIndex > 0 ? 'row' : 'row-reverse'}`
+          }}
+        >
+          <img
+            src={images[resetIndex(Math.abs(index))]}
+            alt="current"
+            className="w-full h-full"
+          />
+          <img
+            src={images[resetIndex(Math.abs(index + nextIndex))]}
+            alt="current"
+            className="w-full h-full"
+          />
+        </div>
+      </div>
+      <div className="w-full h-12 space-x-2 p-2 flex flex-row bg-gray-700 rounded-b-lg">
+        <button
+          className="flex-grow rounded-lg bg-black text-white"
+          onClick={goLeft}
+        >
+          {"<"}
+        </button>
+        <button
+          className="flex-grow rounded-lg bg-black text-white"
+          onClick={goRight}
+        >
+          {">"}
+        </button>
+      </div>
+      {index}:{index + nextIndex}
+    </div>
+  );
+}
+
+export default Carrousel;
